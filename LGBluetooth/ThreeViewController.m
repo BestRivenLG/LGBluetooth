eeb694785924821f89a1c177e8dd10dc5add9f58
BestRivenLG <373268232@qq.com>
2016-06-02 14:01:24 +0800

    LGBluetooth

--- LGBluetooth/ThreeViewController.m
+++ LGBluetooth/ThreeViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ThreeViewController.h"
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
 
-@interface ThreeViewController ()
+@interface ThreeViewController ()<MCSessionDelegate,MCBrowserViewControllerDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong,nonatomic) MCSession *session;
+@property (strong,nonatomic) MCBrowserViewController *browserController;
+@property (strong,nonatomic) UIImagePickerController *imagePickerController;
+
 
 @end
 
@@ -16,22 +22,101 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.navigationItem.title = @"发现";
+
+    //创建节点
+    MCPeerID *peerID=[[MCPeerID alloc]initWithDisplayName:@"KenshinCui"];
+    //创建会话
+    _session=[[MCSession alloc]initWithPeer:peerID];
+    _session.delegate=self;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UI事件
+- (IBAction)searchAction:(id)sender {
+    
+    //开始广播
+    _browserController=[[MCBrowserViewController alloc]initWithServiceType:@"cmj-stream" session:self.session];
+    _browserController.delegate=self;
+    
+    [self presentViewController:_browserController animated:YES completion:nil];}
+
+- (IBAction)selectPhotoAction:(id)sender {
+    
+    _imagePickerController=[[UIImagePickerController alloc]init];
+    _imagePickerController.delegate=self;
+    [self presentViewController:_imagePickerController animated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - MCBrowserViewController代理方法
+-(void)browserViewControllerDidFinish:(MCBrowserViewController *)browserViewController{
+    NSLog(@"已选择");
+    [self.browserController dismissViewControllerAnimated:YES completion:nil];
 }
-*/
+-(void)browserViewControllerWasCancelled:(MCBrowserViewController *)browserViewController{
+    NSLog(@"取消浏览.");
+    [self.browserController dismissViewControllerAnimated:YES completion:nil];
+}
 
+#pragma mark - MCSession代理方法
+-(void)session:(MCSession *)session peer:(MCPeerID *)peerID didChangeState:(MCSessionState)state{
+    NSLog(@"didChangeState");
+    switch (state) {
+        case MCSessionStateConnected:
+            NSLog(@"连接成功.");
+            [self.browserController dismissViewControllerAnimated:YES completion:nil];
+            break;
+        case MCSessionStateConnecting:
+            NSLog(@"正在连接...");
+            break;
+        default:
+            NSLog(@"连接失败.");
+            break;
+    }
+}
+//接收数据
+-(void)session:(MCSession *)session didReceiveData:(NSData *)data fromPeer:(MCPeerID *)peerID{
+    NSLog(@"开始接收数据...");
+    UIImage *image=[UIImage imageWithData:data];
+    [self.imageView setImage:image];
+    //保存到相册
+    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    
+}
+
+- (void)session:(MCSession *)session didStartReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID withProgress:(NSProgress *)progress
+{
+    NSLog(@"didStartReceivingResourceWithName");
+
+}
+
+- (void)session:(MCSession *)session didReceiveStream:(NSInputStream *)stream withName:(NSString *)streamName fromPeer:(MCPeerID *)peerID
+{
+    NSLog(@"didReceiveStream");
+
+}
+
+- (void)session:(MCSession *)session didFinishReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID atURL:(NSURL *)localURL withError:(nullable NSError *)error
+{
+    NSLog(@"didFinishReceivingResourceWithName");
+
+}
+
+#pragma mark - UIImagePickerController代理方法
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    UIImage *image=[info objectForKey:UIImagePickerControllerOriginalImage];
+    
+    [self.imageView setImage:image];
+    //发送数据给所有已连接设备
+    NSError *error=nil;
+    [self.session sendData:UIImagePNGRepresentation(image) toPeers:[self.session connectedPeers] withMode:MCSessionSendDataUnreliable error:&error];
+    NSLog(@"开始发送数据...");
+    if (error) {
+        NSLog(@"发送数据过程中发生错误，错误信息：%@",error.localizedDescription);
+    }
+    [self.imagePickerController dismissViewControllerAnimated:YES completion:nil];
+}
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [self.imagePickerController dismissViewControllerAnimated:YES completion:nil];
+}
 @end
--- LGBluetooth/ViewController.m
+++ LGBluetooth/ViewController.m
@@ -7,21 +7,90 @@
 //
 
 #import "ViewController.h"
+#import <GameKit/GameKit.h>
+#import "TwoViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<GKPeerPickerControllerDelegate,UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong,nonatomic) GKSession *session;//蓝牙连接会话
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+ 
+    GKPeerPickerController *pearPickerController = [[GKPeerPickerController alloc] init];
+    pearPickerController.delegate = self;
+//    [pearPickerController show];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 选择图片
+- (IBAction)selectedAction:(id)sender {
+    
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+    [self presentViewController:imagePicker animated:YES completion:nil];
+    
 }
+
+#pragma mark - 发送图片
+- (IBAction)sendAction:(id)sender {
+    NSData *data = UIImagePNGRepresentation(self.imageView.image);
+    NSError *error = nil;
+    
+    [self.session sendDataToAllPeers:data withDataMode:GKSendDataReliable error:&error];
+    if (error) {
+        NSLog(@"发送图片过程中发生错误，错误信息:%@",error.localizedDescription);
+    }
+}
+
+
+#pragma mark - UIImagePickerController代理方法
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    self.imageView.image = info[UIImagePickerControllerOriginalImage];
+    [self dismissViewControllerAnimated:YES completion:nil];
+
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - GKPeerPickerController代理方法
+/**
+ *  连接到某个设备
+ *
+ *  @param picker  蓝牙点对点连接控制器
+ *  @param peerID  连接设备蓝牙传输ID
+ *  @param session 连接会话
+ */
+- (void)peerPickerController:(GKPeerPickerController *)picker didConnectPeer:(NSString *)peerID toSession:(GKSession *)session
+{
+    NSLog(@"已连接客户端设备:%@.",peerID);
+    //设置数据接收处理句柄，相当于代理，一旦数据接收完成调用它的-receiveData:fromPeer:inSession:context:方法处理数据
+    [self.session setDataReceiveHandler:self withContext:nil];
+    
+    [picker dismiss];//一旦连接成功关闭窗口
+}
+
+#pragma mark - 蓝牙数据接收方法
+
+//- (void)setDataReceiveHandler:(id)handler withContext:(void *)context
+- (void) receiveData:(NSData *)data fromPeer:(NSString *)peer inSession: (GKSession *)session context:(void *)context
+{
+    UIImage *image=[UIImage imageWithData:data];
+    self.imageView.image=image;
+    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    NSLog(@"数据发送成功！");
+}
+
+
+
+
+
+
 
 @end
